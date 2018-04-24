b6cb58070abd8048b4f9e7bae50afaefcee02b98
Jeremy Breidenbach <jeremy.breidenbach@gmail.com>
2018-04-23 21:21:37 -0500

    Fixed scrolling issue when campsite list loaded in ui

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './fontawesome-all.min.js';
+import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
-        <App />
+        <ScrollToTop>
+            <App />
+        </ScrollToTop>
     </BrowserRouter>
 ),document.getElementById('root'));
 registerServiceWorker();
